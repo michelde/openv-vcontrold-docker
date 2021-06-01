6f9d8d8326d1c5080636fee15d2d777a7366a961
Michel Munzert <michel@munzert.berlin>
2021-06-01 20:16:19 +0200

    updates

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM debian:stable-slim
 
 RUN apt-get update && \
     apt-get upgrade -y && \
-    apt-get install -y build-essential vim subversion automake autoconf telnet libxml2-dev mosquitto-clients git cmake python-docutils
+    apt-get install -y build-essential vim subversion automake autoconf telnet libxml2-dev mosquitto-clients git cmake python-docutils jq
 RUN mkdir openv && cd openv && \
     git clone https://github.com/openv/vcontrold.git vcontrold-code && \
     cd vcontrold-code && \
---
+++ config/mqtt_sub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script subscribes to a MQTT topic using mosquitto_sub.
+# On each message received, you can execute whatever you want.
+
+while true  # Keep an infinite loop to reconnect when connection lost/broker unavailable
+do
+    mosquitto_sub -u $MQTTUSER -P $MQTTPASSWORD -h $MQTTHOST -p $MQTTPORT -t $MQTTTOPIC/setTempA | while read -r payload
+    do
+        # Here is the callback to execute whenever you receive a message:
+        echo "Rx MQTT: ${payload}"
+        vclient -h 127.0.0.1 -p 3002 
+    done
+    sleep 10  # Wait 10 seconds until reconnection
+done &  # Discomment the & to run in background (but you should rather run THIS script in background)
