61263b89e225ba1a1a3c367fadeb4e00c5a834d3
Michel Munzert <michel@munzert.berlin>
2022-02-22 20:17:04 +0100

    ...

--- config/mqtt_publish.sh
+++ config/mqtt_publish.sh
@@ -2,5 +2,7 @@
 jq -c '.[]' result.json | while read i; do
     COMMAND=$(echo $i | jq -r ' .command')
     RAW=$(echo $i | jq -r ' .value')
-    mosquitto_pub -u $MQTTUSER -P $MQTTPASSWORD -h $MQTTHOST -p $MQTTPORT -t $MQTTTOPIC/$COMMAND -m $RAW
+    PAYLOAD=$(echo $i | jq -r '. ')
+    #echo $PAYLOAD
+    mosquitto_pub -u $MQTTUSER -P $MQTTPASSWORD -h $MQTTHOST -p $MQTTPORT -t $MQTTTOPIC/$COMMAND -m "$PAYLOAD"
 done
